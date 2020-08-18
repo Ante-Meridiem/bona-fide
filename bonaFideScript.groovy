d8449f7a334251533d8b5a3273e9df1a992b172f
bonafide2020 <65115184+bonafide2020@users.noreply.github.com>
2020-08-19 01:00:00 +0530

    Update bonaFideScript.groovy

--- bonaFideScript.groovy
+++ bonaFideScript.groovy
@@ -7,22 +7,22 @@ def getReleaseConfirmation() {
     }
 }
 
-def getBuildVersion(){
+def getBuildVersion() {
     git credentialsId: 'bona-fide', url: 'git@github.com:Ante-Meridiem/Bona-Fide.git'
     def masterCommit = sh(returnStdout: true, script: "git log -n 1 --pretty=format:'%h'").trim()
     def currentDate = sh(returnStdout: true, script: 'date +%Y-%m-%d').trim()
     env.BUILD_VERSION = currentDate + "-" + masterCommit
 }
 
-def fecthJarAndDockerFile(){
+def fecthJarAndDockerFile() {
     def fetchErrorMessage = 'Error while fetching the Jar and dockerfile'
-    try{
+    try {
         sh label: 'createTargetDirectory', script: '''sudo mkdir -p target'''
         sh label: 'copyJarFile', script: '''sudo cp /home/ec2-user/Bona-Fide/bona-fide.jar target'''
         sh label: 'copyDockerFile', script: 'sudo cp /home/ec2-user/Bona-Fide/Dockerfile .'
     }
-    catch(Exception e){
-        echo "${fetchErrorMessage} ${e.getMessage()}"
+    catch (Exception e) {
+        error "${fetchErrorMessage} ${e.getMessage()}"
     }
 }
 
@@ -32,13 +32,47 @@ def buildDockerImage() {
         sh "docker build -f Dockerfile -t docker4bonafide/bona-fide/${buildVersion} ."
     }
     catch (Exception e) {
-        echo "${dockerImgBuildError} ${e.getMessage()}"
+        error "${dockerImgBuildError} ${e.getMessage()}"
     }
 }
 
-def pushDockerImage(){
+def pushDockerImage() {
+    def dockerImagePushError = 'Error while pushing docker image'
     withCredentials([string(credentialsId: 'docker-hub-password-bona-fide', variable: 'bonaFideDockerHubPassword')]) {
         sh "docker login -u docker4bonafide -p ${bonaFideDockerHubPassword}"
+    }
+    try {
+        sh "docker push docker4bonafide/bona-fide/${buildVersion}"
+    }
+    catch (Exception e) {
+        error "${dockerImagePushError} ${e.getMessage()}"
+    }
+}
+
+def stopRunningContainer(){
+    echo 'Hi'
+}
+
+def runContainer(){
+    def dockerContainerRunError = 'Error while running the container'
+    sshagent(['bonaFideDeploymentAccess']) {
+        try{
+            sh "docker run -d -p 9002:9002 --name bona_fide_container docker4bonafide/bona-fide/${buildVersion}"
+        }
+        catch(Exception e){
+            error "${dockerContainerRunError} ${e.getMessage()}"
+        }
+        echo 'Waiting for a minute...'
+        sleep 59
+    }
+}
+
+def performHealthCheck(){
+    def httpResponseStatus = "HTTP/1.1 200"
+    final String url = 'http://ec2-13-126-97-24.ap-south-1.compute.amazonaws.com:9002/bona-fide/base/version'
+    final String response = sh(script: "curl -Is $url | head -1", returnStdout: true).trim()
+    if(response == "${httpResponseStatus}"){
+        APPLICATION_RUNNING_STATUS = true
     }
 }
 
